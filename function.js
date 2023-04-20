f249ed76336c198e8b29d01525c1861bc6915b24
shashank7200 <126809978+shashank7200@users.noreply.github.com>
2023-04-20 12:41:14 +0530

    added initial file

---
+++ function.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image = document.querySelectorAll("img")[0];
+
+image.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomeImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomeImageSource2);
+
+
+//if player 1 win
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 win";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
